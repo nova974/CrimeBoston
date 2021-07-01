c35d645af4630eaa757f0be9478cfd2ee047c7a4
Dorian TONA <tonad@fr.glory-global.com>
2021-07-01 15:27:16 +0200

    feature(filter): added filter for offense code

--- server/index.js
+++ server/index.js
@@ -10,7 +10,14 @@ app.get('/', (req, res) => {
 app.get('/crimes', async (req, res) => {
     const response = await fetch("https://data.boston.gov/api/3/action/datastore_search?resource_id=be047094-85fe-4104-a480-4fa3d03f9623")
     const json = await response.json()
-    res.send(json.result.records)
+
+    const offense_code = req.query.offense_code
+    if(offense_code != undefined)
+    {
+        const filtered = json.result.records.filter(json => json.OFFENSE_CODE == offense_code)
+        return res.send(filtered)
+    }
+    return res.send(json.result.records)
 });
 
 app.listen(port, () => {
