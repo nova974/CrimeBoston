f6f98deae85a8596fbefc2f40dc96e3fc048db21
William Gossard <gossard.william@gmail.com>
2021-07-01 16:57:21 +0200

    :tada: first client commit

---
+++ client/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>{{ name }} {{ counter }}</h1>
+        <button @click="increment()">Ajouter</button>
+        <button @click="decrement()">Supprimer</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String
+    },
+    data: function() {
+        return  {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter++
+        },
+        decrement: function() {
+            this.counter--
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
